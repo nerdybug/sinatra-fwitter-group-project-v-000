5a88e387c63ef57e22e17e1ef0cdc373a3423806
Meredith Reece <mardebugger@gmail.com>
2018-03-14 02:13:53 +0000

    add login routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,6 +31,29 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets' do
+    @user = User.find_by(id: session[:user_id])
+    erb :'tweets/tweets'
+  end
+
   helpers do
     def logged_in?
       !!session[:user_id]
